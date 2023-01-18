36f7d9da10d58a2ed3f9cfa28a4c8a51958ccc07
“ndaloben” <ndaloben8@gmail.com>
2023-01-18 13:16:17 +0300

    Adaptive header section: Header section changes color based on the active page

--- src/components/common/header/Header.jsx
+++ src/components/common/header/Header.jsx
@@ -1,63 +1,74 @@
 import React, { useState } from 'react'
+import { NavLink, useLocation } from 'react-router-dom';
 import './header.css'
 import Logo from '../../images/logo.png'
-import { Link } from 'react-router-dom'
 import Bars from '../../images/bars.png'
 const Header = () => {
   const mobile = window.innerWidth<=768 ? true:false;
   const [menuOpened, setMenuOpened] =  useState(false)
   return (
     <div className='header'>
-    <Link to='/'>
+       <nav
+    style={{
+      background: useLocation().pathname !== "/" ? "rgba(2, 2, 2, 0.73)" : ""
+    }}
+  >
+    <NavLink to='/'>
      <img src={Logo} alt=""  className='logo'/>
-     </Link>
+     </NavLink>
     <div className="blur hero-blur"></div>
 {menuOpened=== false && mobile===true ? (
   <div style={{backgroundColor: 'white', padding: '0.5rem', borderRadius: '5px'}}
   
   onClick={() => setMenuOpened(true)}
   >
-    <img src={Bars} alt="" className='bars' style={{width: '1.5rem', height: '1.5rem'}}/>
+    <img src={Bars} alt="" className='bars' style={{ width: '1.5rem', height: '1.5rem'}}/>
     </div>
 ) : (
 <ul className='header-menu'>
 <li> 
-<Link onClick={()=> setMenuOpened(false)}
+<NavLink exact activeClassName="active"
+onClick={()=> setMenuOpened(false)}
 to='about'
 span={true}
-smooth={true}>About Us</Link> 
+smooth={true}>About Us</NavLink> 
 </li>
 
 <li>
-<Link onClick={()=> setMenuOpened(false)}
+<NavLink exact activeClassName="active"
+onClick={()=> setMenuOpened(false)}
 to='programs'
 span={true}
-smooth={true}>Programs</Link> 
+smooth={true}>Programs</NavLink> 
 </li>
 
 <li>
-<Link onClick={()=> setMenuOpened(false)}
+<NavLink exact activeClassName="active" 
+onClick={()=> setMenuOpened(false)}
 to='cohorts'
 span={true}
-smooth={true}>Cohorts</Link>
+smooth={true}>Cohorts</NavLink>
 </li>
 
 <li>
-<Link onClick={()=> setMenuOpened(false)}
+<NavLink exact activeClassName="active"
+onClick={()=> setMenuOpened(false)}
 to='pages'
 span={true}
-smooth={true}>Startup care</Link>
+smooth={true}>Startup care</NavLink>
 </li>
 
 <li>
-<Link onClick={()=> setMenuOpened(false)}
+<NavLink exact activeClassName="active" 
+ onClick={()=> setMenuOpened(false)}
 to='news'
 span={true}
-smooth={true}><span>News</span></Link>
+smooth={true}><span>News</span></NavLink>
 </li>
 <button className='btn'>Enroll Now</button>
 </ul>
-)}   
+)} 
+</nav>
     </div>
   );
 };
