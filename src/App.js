23952fe5e86d1f27bf54279fb32c1fbe8fa5b422
NLornah <nderitulornah040@gmail.com>
2023-01-19 15:02:33 +0300

    implementation

--- src/App.js
+++ src/App.js
@@ -4,18 +4,17 @@ import Home from './components/home/Home';
 import Startup from './components/common/startup/Startup';
 import Header from './components/common/header/Header';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Footer from './components/footer/Footer';
+import Enroll from './components/enroll/Enroll';
+import News from './components/news/News';
 
 
 function App ()  {
   return (
       <>
-   <Router>
-    <Header />
-    <Routes>
-    <Route path='/' element={<Home />} />
-    <Route path='/startup' element={<Startup />} />
-    </Routes>
-    </Router>         
+   <Footer/>  
+   <Enroll/>
+   <News/>
     </>
   )
 }
