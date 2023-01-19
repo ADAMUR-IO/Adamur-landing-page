4689d8b1d98ef1d49db51609beb0cb887370af4f
NLornah <nderitulornah040@gmail.com>
2023-01-20 01:44:59 +0300

    testimonials

--- src/App.js
+++ src/App.js
@@ -7,14 +7,16 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Footer from './components/footer/Footer';
 import Enroll from './components/enroll/Enroll';
 import News from './components/news/News';
+import Cards from './components/cards/Cards';
 
 
 function App ()  {
   return (
       <>
-   <Footer/>  
-   <Enroll/>
-   <News/>
+   {/* <Footer/>   */}
+   <Cards/>
+   {/* <Enroll/>
+   <News/> */}
     </>
   )
 }
