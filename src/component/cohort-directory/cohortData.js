83dc69c65ebe49aed75b09fe08cf19daec415305
innocentadika <innocentadika18@gmail.com>
2023-01-23 12:45:56 +0300

    addition of Robin and Lornah image on cohort pannel

--- src/component/cohort-directory/cohortData.js
+++ src/component/cohort-directory/cohortData.js
@@ -1,17 +1,20 @@
+import Lornah from '../../component/images/cohort/Lornah.png';
+import Robin from '../../component/images/cohort/Robin.png';
 import Group from '../../component/images/Group.png';
+
 
 
 
 export const clientData = [
     {
-    image: Group,
+    image: Lornah,
     review: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. A fringilla at gravida justo, felis risus ullamcorper augue tempus. Nam hac.',
-    name: 'Client 1'
+    name: 'Lornah'
     },
     {
-        image: Group,
+        image: Robin,
         review: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. A fringilla at gravida justo, felis risus ullamcorper augue tempus. Nam hac.',
-    name: 'Client 1'
+    name: 'Robin'
     },
     {
         image: Group,
