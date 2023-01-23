8c0d8b8e475fff00b6736e83caee349b501b798d
innocentadika <innocentadika18@gmail.com>
2023-01-23 12:27:34 +0300

    updation of the current arrrows section-2 loader

--- src/component/Section-2/Section-2.component.jsx
+++ src/component/Section-2/Section-2.component.jsx
@@ -7,7 +7,7 @@ function Section() {
 
     <div className=' bg-black min-h-screen xl:h-fit'>
         
-        <div className='text-white items-center justify-between p-3 xl:p-6'>
+        <div className='text-white items-center justify-between p-2 xl:p-6'>
 
         <div className='flex space-x-4 justify-center xl:mb-20'>
                     
@@ -18,7 +18,7 @@ function Section() {
                     
                             <span className=' mt-8 font-medium text-sm xl:text-2xl'>We bring well trained and delivery ready product engineers</span>
                                     </div>
-        <img className='ml-64 w-24 xl:w-56 xl:float-right xl:mr-4 ' src={Dot} alt="dots" />
+        <img className='ml-60 w-24 xl:w-56 xl:float-right xl:mr-4 ' src={Dot} alt="dots" />
 
                                     <div className='text-start xl:text-xl xl:ml-56 xl:w-96 xl:mt-16'>
                                     <p className='pb-6'>At Adamur we make work easier by</p>
@@ -27,11 +27,11 @@ function Section() {
                                     </div>
                                     <div className=' w-28 space-x-0 ml-32 gap-10 xl:w-40 xl:float-right xl:mr-80 '>
                                         <div className='relative flex'>
-                                        <img className='animate-bounce delay-75' src={Puzzle1} alt="second" />
+                                        <img className='animate-bounce delay-75 object-fill scale-50 ' src={Puzzle1} alt="second" />
                                         <img className='animate-bounce delay-150 ' src={Puzzle2} alt="third" />
                                         </div>
                                         <div className=' relative flex '>
-                                        <img className='animate-bounce animate-pulse' src={Puzzle3} alt="first" />
+                                        <img className='animate-bounce animate-pulse object-fill' src={Puzzle3} alt="first" />
                                         <img className='animate-bounce delay-100' src={Puzzle4} alt="forth" />
                                         </div>
                                         <p className='text-sm xl:text-lg'>Nothing great is made alone</p>
