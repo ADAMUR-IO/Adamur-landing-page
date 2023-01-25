87048575b356d82aff6a801bdef8218a856969a1
“ndaloben” <ndaloben8@gmail.com>
2023-01-25 13:06:33 +0300

    updating the startup page

--- src/components/pages/Pages.jsx
+++ src/components/pages/Pages.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import Navigation from '../navigation/Navigation'
 import Startups from '../startup/Startups'
 import Startup from '../startup-care/startup-care.component'
 import Cards from '../cards/Cards'
@@ -9,7 +8,6 @@ import Footer from '../common/footer/Footer'
 const Pages = () => {
   return (
     <>
-    <Navigation />
      <Startups />
      <Startup /> 
      <Cards />
